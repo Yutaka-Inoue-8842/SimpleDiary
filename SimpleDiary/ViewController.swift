dc64213b5237d798ffbaac882a95945a3c03e6b9
YutakaInoue <inoue.8842@gmail.com>
2023-02-25 16:05:59 +0900

    initial commit

---
+++ SimpleDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleDiary
+//
+//  Created by Yutaka Inoue on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
